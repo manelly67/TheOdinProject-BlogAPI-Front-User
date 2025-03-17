71e703c4fa97f221d73c5dc9093b3b3fdebc631e
manellylopez <manellylopezqqq@gmail.com>
2025-03-17 14:50:00 -0500

    ErrorPage component error element route

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,18 +1,19 @@
 import App from "../App";
 import Details from "./Details";
+import ErrorPage from "./Error_page";
 
 const routes = [
-    {
-      index: true,
-      path: "/",
-      element: <App />,
-  
-     /*  errorElement: <ErrorPage />, */
-    },
-    {
-      path: "posts/:authorid/:postid",
-      element: <Details />,
-    },
+  {
+    index: true,
+    path: "/",
+    element: <App />,
+
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "posts/:authorid/:postid",
+    element: <Details />,
+  },
 ];
 
 export default routes;
