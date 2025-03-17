b8467e74d51ae0ca63fe3c34b0a24ec1993e636f
manellylopez <manellylopezqqq@gmail.com>
2025-03-17 11:48:15 -0500

    initial router in main.jsx

---
+++ src/components/routes.jsx
@@ -0,0 +1,13 @@
+import App from "../App";
+
+const routes = [
+    {
+      index: true,
+      path: "/",
+      element: <App />,
+  
+     /*  errorElement: <ErrorPage />, */
+    },
+];
+
+export default routes;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import routes from "./components/routes.jsx";
 
-createRoot(document.getElementById('root')).render(
+
+const meta = document.createElement('meta');
+meta.setAttribute('name','description');
+meta.setAttribute('content','manelly67\'s exercise study project:BLOG API within the curriculum The Odin Project');
+document.head.appendChild(meta);
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
